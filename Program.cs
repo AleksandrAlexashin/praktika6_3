8c728c9d28940683a6b52d1e1db13ae7735d8770
AleksandrAlexashin <sashokmetall@mail.ru>
2021-12-11 00:25:27 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Введите предложение содержащее слово в фигурных скобках: ");
+            string s = Console.ReadLine();
+            string s2 = System.Text.RegularExpressions.Regex.Replace (s, @"{[\d\D]+}", "");
+            Console.WriteLine(s);
+                                
+            Console.WriteLine(s2);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
